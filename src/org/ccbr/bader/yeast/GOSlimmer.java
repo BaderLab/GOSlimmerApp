8adbb5dfc603df36f4554e72205b9f0222ed6250
Mike Matan <mmatan>
2007-04-11 19:26:45 +0000

    added new public static field 'ontologyName' for storing the name of the ontology which GOSlimmer is to work on. This is needed by code which instantiates GeneAssociationReaderUtil, since it requires the ontology which is associated with the gene association file to be parsed.

--- src/org/ccbr/bader/yeast/GOSlimmer.java
+++ src/org/ccbr/bader/yeast/GOSlimmer.java
@@ -15,4 +15,6 @@ public class GOSlimmer {
 	
 	public static final Map<CyNetwork,GOSlimmerCoverageStatBean> networkToCoverageStatMap = new HashMap<CyNetwork, GOSlimmerCoverageStatBean>();
 	
+	/*This will record the name of the ontology which GOSlimmer is working on, as derived from the name of the network being worked on*/
+	public static String ontologyName;
 }
