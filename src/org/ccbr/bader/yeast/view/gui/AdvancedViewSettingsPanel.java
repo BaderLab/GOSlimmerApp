ffd164cc8bf9f7bbe690a171bdca3a63fec09cba
Mike Matan <mmatan>
2007-04-11 20:39:26 +0000

    added new checkbox for enabling/disabling labelling of nodes with their ontology term name instead of the ID enabled redrawing of the graph after such a view settings change.

--- src/org/ccbr/bader/yeast/view/gui/AdvancedViewSettingsPanel.java
+++ src/org/ccbr/bader/yeast/view/gui/AdvancedViewSettingsPanel.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import cytoscape.Cytoscape;
+import cytoscape.view.CyNetworkView;
 
 public class AdvancedViewSettingsPanel extends JPanel implements ActionListener {
 
@@ -37,26 +38,43 @@ public class AdvancedViewSettingsPanel extends JPanel implements ActionListener 
 		initComponents();
 	}
 	
-	JLabel includeDescendentCoverageInNodeSizeCalculationLabel;
 	JCheckBox includeDescendentCoverageInNodeSizeCalculationCheckBox;
 	private final String includeDescendentCoverageInNodeSizeCalculationText = "Include child nodes when calculating node size";
+	
+	private final String lsep = System.getProperty("line.separator");
 	
 	private JCheckBox getIncludeDeCheckBox() {
 		if (includeDescendentCoverageInNodeSizeCalculationCheckBox==null) {
 			includeDescendentCoverageInNodeSizeCalculationCheckBox = new JCheckBox(includeDescendentCoverageInNodeSizeCalculationText);
 			includeDescendentCoverageInNodeSizeCalculationCheckBox.setToolTipText("If checked, then the size of the node will be proportional to the number of genes"
-					+ " directly annotated by the node + the number of genes annotated by all of the nodes descendants in the graph. "
-					+ " If unchecked, size is proportional to the directly annotated nodes only.");
+					+ lsep +" directly annotated by the node + the number of genes annotated by all of the nodes descendants in the graph. "
+					+ lsep + " If unchecked, size is proportional to the directly annotated nodes only.");
 			includeDescendentCoverageInNodeSizeCalculationCheckBox.addActionListener(this);
 		}
 		return includeDescendentCoverageInNodeSizeCalculationCheckBox;
 	}
 	
+	JCheckBox labelNodesWithOntologyName;
+	private final String labelNodesWithOntologyNameLabel = "Label nodes with Ontology Term Name";
+	
+	private JCheckBox getLCheckBox() {
+		if (labelNodesWithOntologyName==null) {
+			labelNodesWithOntologyName = new JCheckBox(labelNodesWithOntologyNameLabel);
+			labelNodesWithOntologyName.setToolTipText("If checked, nodes will be labelled with their biologically meaningful ontology term name."
+					+ lsep + "  Otherwise, they will be labelled with their GO ID.");
+			labelNodesWithOntologyName.addActionListener(this);
+		}
+		return labelNodesWithOntologyName;
+	}
+	
 	private void initComponents() {
 		this.setBorder(BorderFactory.createTitledBorder("Advanced View Settings"));
 		this.add(getIncludeDeCheckBox());
+		this.add(getLCheckBox());
 	}
 
+	
+	
 	public void actionPerformed(ActionEvent event) {
 		Object src = event.getSource();
 		if (src instanceof JCheckBox) {
@@ -64,6 +82,11 @@ public class AdvancedViewSettingsPanel extends JPanel implements ActionListener 
 				GOSlimmerGUIViewSettings.includeDescendantInferredCoveredGenesInNodeSizeCalculations= includeDescendentCoverageInNodeSizeCalculationCheckBox.isSelected();
 				Cytoscape.getVisualMappingManager().applyAppearances();
 			}
+			else if (src == labelNodesWithOntologyName) {
+				GOSlimmerGUIViewSettings.labelNodesWithOntologyName = labelNodesWithOntologyName.isSelected();
+			}
+			CyNetworkView curNet = Cytoscape.getCurrentNetworkView();
+			if (curNet!=null) curNet.redrawGraph(false, true);
 		}
 		
 	}
