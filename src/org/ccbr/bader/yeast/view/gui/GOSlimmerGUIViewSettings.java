e7d13b23e3cb4103443dd2af4261d7833aafee49
Mike Matan <mmatan>
2007-04-11 19:32:46 +0000

    This class will store, through its static field values, the settings which are to be applied to view appearances in the graphs being managed by GOSlimmer, such as how node's are labelled and how their sizes are calculated.

---
+++ src/org/ccbr/bader/yeast/view/gui/GOSlimmerGUIViewSettings.java
@@ -0,0 +1,18 @@
+package org.ccbr.bader.yeast.view.gui;
+
+/**This class will contain global settings for the gui view.  Applications and plugins will be able to programmatically 
+ * modify these settings because they are stored as public static fields.
+ * 
+ * @author mikematan
+ *
+ */
+public class GOSlimmerGUIViewSettings {
+
+	public static int minNodeSize = 0;
+	
+	public static boolean labelNodesWithOntologyName = true;
+	
+	public static boolean includeDescendantInferredCoveredGenesInNodeSizeCalculations = false;
+	
+	
+}
