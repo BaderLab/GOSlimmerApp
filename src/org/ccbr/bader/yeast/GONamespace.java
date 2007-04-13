d76f5712bf228a254c2cacede21e799ac9597bd0
Mike Matan <mmatan>
2007-04-13 18:55:48 +0000

    added new field for specifying the abbreviation used for the namespace in gene annotation files

--- src/org/ccbr/bader/yeast/GONamespace.java
+++ src/org/ccbr/bader/yeast/GONamespace.java
@@ -4,16 +4,25 @@
 package org.ccbr.bader.yeast;
 
 public enum GONamespace{
-	MolFun("Molecular Function","GO:0003674"),
-	BioPro("Biological Process","GO:0008150"),
-	CelCom("Cellular Component","GO:0005575");
+	MolFun("Molecular Function","GO:0003674","F"),
+	BioPro("Biological Process","GO:0008150","P"),
+	CelCom("Cellular Component","GO:0005575","C");
 	
 	private String name;
 	private String rootTermId;
+	private String geneAnnotationAbreviation;
 	
-	GONamespace(String name,String rootTermId) {
+	GONamespace(String name,String rootTermId,String geneAnnotationAbbreviation) {
 		this.name = name;
 		this.rootTermId = rootTermId;
+		this.geneAnnotationAbreviation = geneAnnotationAbbreviation;
+	}
+
+	/**
+	 * @return the abbreviation for this namespace used in gene annotation files
+	 */
+	public String getGeneAnnotationAbreviation() {
+		return geneAnnotationAbreviation;
 	}
 
 	public String getName() {
