8ad0c310447605a1c3c4d19dce4199cb6819cde6
Mike Matan <mmatan>
2007-04-17 20:17:49 +0000

    new exception class for problems during export

---
+++ src/org/ccbr/bader/yeast/export/GOSlimmerExportException.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.ccbr.bader.yeast.export;
+
+import org.ccbr.bader.yeast.GOSlimmerException;
+
+/**
+ * @author mikematan
+ *
+ */
+public class GOSlimmerExportException extends GOSlimmerException {
+
+	/**
+	 * 
+	 */
+	public GOSlimmerExportException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public GOSlimmerExportException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public GOSlimmerExportException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public GOSlimmerExportException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
