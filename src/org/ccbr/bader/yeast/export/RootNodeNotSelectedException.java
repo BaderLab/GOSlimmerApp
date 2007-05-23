9061dcb7a28edca60dcdb0859c35a3dab335d2ad
Mike Matan <mmatan>
2007-05-23 16:09:18 +0000

    new exception class for when root node is not selected upon call to operation which requires such

---
+++ src/org/ccbr/bader/yeast/export/RootNodeNotSelectedException.java
@@ -0,0 +1,24 @@
+package org.ccbr.bader.yeast.export;
+
+public class RootNodeNotSelectedException extends GOSlimmerExportException {
+
+	public RootNodeNotSelectedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RootNodeNotSelectedException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RootNodeNotSelectedException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RootNodeNotSelectedException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
