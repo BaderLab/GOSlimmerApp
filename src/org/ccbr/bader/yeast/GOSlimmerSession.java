7e50212542ed0f83be50f6aa6d1ea494bd54830d
Mike Matan <mmatan>
2007-08-16 20:15:18 +0000

    Added LGPL license, copyright and authorship header, as well as numerous javadoc comments to explain class, field and method meaning.
    Also removed a few obsolete fields and variables, and did some very minor refactoring.

--- src/org/ccbr/bader/yeast/GOSlimmerSession.java
+++ src/org/ccbr/bader/yeast/GOSlimmerSession.java
@@ -1,3 +1,36 @@
+/**
+ * * Copyright (c) 2007 Bader Lab, Donnelly Centre for Cellular and Biomolecular 
+ * * Research, University of Toronto
+ * *
+ * * Code written by: Michael Matan
+ * * Authors: Michael Matan, Gary D. Bader
+ * *
+ * * This library is free software; you can redistribute it and/or modify it
+ * * under the terms of the GNU Lesser General Public License as published
+ * * by the Free Software Foundation; either version 2.1 of the License, or
+ * * any later version.
+ * *
+ * * This library is distributed in the hope that it will be useful, but
+ * * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ * * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ * * documentation provided hereunder is on an "as is" basis, and
+ * * University of Toronto
+ * * has no obligations to provide maintenance, support,
+ * * updates, enhancements or modifications.  In no event shall the
+ * * University of Toronto
+ * * be liable to any party for direct, indirect, special,
+ * * incidental or consequential damages, including lost profits, arising
+ * * out of the use of this software and its documentation, even if
+ * * University of Toronto
+ * * has been advised of the possibility of such damage.  See
+ * * the GNU Lesser General Public License for more details.
+ * *
+ * * You should have received a copy of the GNU Lesser General Public License
+ * * along with this library; if not, write to the Free Software Foundation,
+ * * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * *
+ * * Description: Aggregation class for objects participating in a GOSlimmer session
+ */
 package org.ccbr.bader.yeast;
 
 import giny.model.Edge;
@@ -39,6 +72,12 @@ import cytoscape.visual.VisualMappingManager;
 import cytoscape.visual.VisualStyle;
 import ding.view.NodeContextMenuListener;
 
+/**Aggregates the different objects associated with a given GOSlimmer session, including the subgraph CyNetworks, 
+ * controllers for each subgraph, and the associated model objects.
+ * 
+ * @author mikematan
+ *
+ */
 public class GOSlimmerSession {
 
 	//The Subgraphs of the GO Network representing each of the three sub ontologies of the GO DAG
